a553470f5d423dcfd3937b2e1d38898d0d5e92e3
visveee <vishalveer0324@hotmail.com>
2024-10-21 09:46:18 -0500

    Initial Commit

---
+++ Dollar TriviaTests/Dollar_TriviaTests.swift
@@ -0,0 +1,17 @@
+//
+//  Dollar_TriviaTests.swift
+//  Dollar TriviaTests
+//
+//  Created by Vishal Veeramachaneni on 10/21/24.
+//
+
+import Testing
+@testable import Dollar_Trivia
+
+struct Dollar_TriviaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
